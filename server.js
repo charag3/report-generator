598a4a5f57c3a3f2c1f0ca0978e7f31175cbdd87
charag3 <char.agcreates@outlook.com>
2025-07-09 16:14:19 -0600

    port update

--- server.js
+++ server.js
@@ -492,7 +492,7 @@ app.get('/test-data', (req, res) => {
   res.json(testData);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`PDF service running on port ${PORT}`);
   console.log(`Test data available at: http://localhost:${PORT}/test-data`);
